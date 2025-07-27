18ce18b1f73b2c53a394f96c5083717c011cb083
gabrielbendezu <gabrielbendezu0101@hotmail.com>
2025-07-27 18:12:51 +0200

    Added brush and channel pushes to do real time collab

--- assets/js/components/MapCanvas.jsx
+++ assets/js/components/MapCanvas.jsx
@@ -1,21 +1,62 @@
 import React, { useEffect, useRef } from 'react';
 import * as fabric from 'fabric';
 
+const setupBrush = (canvas, options = {}) => {
+  canvas.isDrawingMode = true;
+  canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
+  canvas.freeDrawingBrush.width = options.width || 5;
+  canvas.freeDrawingBrush.color = options.color || "#000000"
+};
+
 const MapCanvas = ({ channel }) => {
   const canvasRef = useRef(null);
+  const canvasInstanceRef = useRef(null);
 
   useEffect(() => {
     // Create a simple Fabric.js canvas
     const canvas = new fabric.Canvas(canvasRef.current, {
       width: 600,
       height: 400,
-      backgroundColor: '#0099FF'
-    });
-    
-    canvas.renderAll();
+      backgroundColor: '#002080'
+    })
 
+    canvasInstanceRef.current = canvas;
+    setupBrush(canvas, {});
+    
+    //canvas.renderAll();
+
+    // Receive brush strokes
     channel.on("canvas_update", payload => {
-      // Handle canvas updates from other users
+      switch(payload.type) {
+        case "brush_stroke":
+          const path = new fabric.Path(payload.data.path, {
+            stroke: payload.data.stroke,
+            strokeWidth: payload.data.strokeWidth,
+            fill: payload.data.fill
+          });
+          canvas.add(path);
+          canvas.renderAll();
+          //break;
+      }
+
+    });
+
+    // Send brush strokes
+    // TODO: Maybe the channel(or a separate thing) should keep some mapcanvas data for everyone, which includes
+    // everyones current brush settings, such that the brush settings are always communicated when they are set by someone
+    // and not everytime someone draws.
+    canvas.on("path:created", (e) => {
+      const pathData = {
+        path: e.path.path,
+        stroke: e.path.stroke,
+        strokeWidth: e.path.strokeWidth,
+        fill: e.path.fill
+      };
+
+      channel.push("canvas_draw", {
+        type: "brush_stroke",
+        data: pathData
+      });
     });
 
     // Clean up when component unmounts
@@ -23,7 +64,7 @@ const MapCanvas = ({ channel }) => {
       canvas.dispose();
       channel.off("canvas_update");
     };
-  }, []);
+  }, [channel]);
 
   return (
     <div>
